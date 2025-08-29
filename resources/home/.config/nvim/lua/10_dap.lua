93a9abced145b67da76e12201d12de681de9458c
Kael D'Alcamo <dev@kael-k.io>
2025-08-29 11:07:49 +0200

    removed home directory setup, dotfiles moved to dedicated repository

--- resources/home/.config/nvim/lua/10_dap.lua
+++
@@ -1,4 +0,0 @@
----- DAP
--- vimspector setup
-vim.g.vimspector_enable_mappings = 'VISUAL_STUDIO'
-vim.g.vimspector_install_gadgets = { 'debugpy', 'vscode-cpptools', 'delve', 'vscode-bash-debug', 'vscode-firefox-debug', 'vscode-php-debug', 'vscode-java-debug' }
--- resources/home/.config/nvim/lua/10_nvim_treesitter.lua
+++
@@ -1,8 +0,0 @@
-require('nvim-treesitter.configs').setup {
-  ensure_installed = { "python", "lua", "json", "bash", "yaml", "markdown", "html", "css", "rust", "c", "cpp", "javascript"},
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-}
-
